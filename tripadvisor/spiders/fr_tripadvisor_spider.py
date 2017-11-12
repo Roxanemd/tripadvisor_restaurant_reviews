1cab7efd4cf76116ddf492cd88378923ed47e115
Roxanemd <roxane_desrousseaux@hotmail.com>
2017-11-12 12:19:50 +0100

    Add files via upload

--- tripadvisor/spiders/fr_tripadvisor_spider.py
+++ tripadvisor/spiders/fr_tripadvisor_spider.py
@@ -17,7 +17,6 @@ class TripAdvisor(Spider):
     	self.nb_per_page = 10
     
     def parse(self, response):
-	print response.url
 	self.driver.get(response.url)
 	# pour avoir le  commentaire en entier
 	next = self.driver.find_element_by_xpath('//span[text()="Plus"]')
@@ -26,10 +25,11 @@ class TripAdvisor(Spider):
 	reviews = sel.xpath('//div[@class="wrap"]')
 	for rev in reviews :
 		title=rev.xpath('.//div/a/span/text()').extract_first()
-		discussion = rev.xpath('.//div[@class="prw_rup prw_reviews_text_summary_hsx"]//text()').extract()
+		rating=rev.xpath('.//div[@class="rating reviewItemInline"]/span/@class').extract_first()
+    		discussion = rev.xpath('.//div[@class="prw_rup prw_reviews_text_summary_hsx"]//text()').extract()
+		#print discussion
 		raw_txt = ' '.join(discussion)
 		content = raw_txt.split('Afficher moins')[0].strip() # response of hotel could be added here
-		rating=rev.xpath('.//div[@class="rating reviewItemInline"]/span/@class').extract_first()
 		date_=rev.xpath('.//div[@class="rating reviewItemInline"]/span/text()').extract()
 		if len(date_)==1:
 		    if re.search('(ago)$', date_[0]) != None:
@@ -49,11 +49,12 @@ class TripAdvisor(Spider):
 	self.nb_per_page += 10
 	next_p = 'https://www.tripadvisor.com/Restaurant_Review-g187147-d719052-Reviews-or%d-Epicure-Paris_Ile_de_France.html'%(self.nb_per_page)
 	yield Request(next_p, callback=self.parse)
+		
 
 
 
-    """def closed(self,reason):
-        self.driver.close()"""
+    def closed(self,reason):
+        self.driver.close()
                 
                 
                 
